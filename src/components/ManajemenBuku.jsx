f0707ae7914b6442de9633f76cb6a21f1a325499
Muhamad Tajudin Subki <mtajudin98@gmail.com>
2022-03-26 14:39:18 +0700

    Add Components

---
+++ src/components/ManajemenBuku.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ManajemenBuku(){
+    return(
+        <div className="container mt-3">
+            <h1 className="text-center">Manajemen Buku</h1>
+            <button className="btn btn-primary my-2">
+                Tambah Buku
+            </button>
+            {/* input form */}
+            <div className="form">
+
+            </div>
+            {/* tabel data buku */}
+        </div>
+    )
+}
+export default ManajemenBuku;
